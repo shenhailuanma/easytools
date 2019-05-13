f7850d74eab80632d08bd399bf5daa8e604cb773
zhangxu <zx051212@gmail.com>
2019-05-14 02:05:22 +0800

    config

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,102 +1,62 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    
+    <div
+          style="margin-top: 8px;
+                    white-space: pre-line;
+                    padding: 8px;
+                    background-color: black;
+                    font-size:14px;
+                    color: white;height: 580px;overflow-y: scroll; width:960px;word-wrap:break-word;text-align:left"
+        >{{stdout}}</div>
+
+    
+    <p>stderr:</p>
+     {{stderr}}
   </div>
 </template>
 
 <script>
+import FFmpeg from "ffmpeg.js/ffmpeg-mp4.js";
+
 export default {
-  name: 'HelloWorld',
-  data () {
+  name: "HelloWorld",
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: "Welcome to Your Vue.js App",
+      stdout:"",
+      stderr:""
+    };
+  },
+  methods:{
+    ffmpegVersion() {
+      // var ffmpeg = require("ffmpeg.js/ffmpeg-mp4.js");
+      
+      var _this = this;
+      // Print FFmpeg's version.
+      FFmpeg({
+        arguments: ["-codecs"],
+        print: function(data) { _this.stdout += data + "\n"; },
+        printErr: function(data) { _this.stderr += data + "\n"; },
+        onExit: function(code) {
+          console.log("Process exited with code " + code);
+          console.log(_this.stdout);
+        },
+      });
     }
+  },
+  mounted: function() {
+    console.log("挂载到dom后");
+    this.ffmpegVersion();
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
